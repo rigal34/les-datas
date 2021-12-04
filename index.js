c232065fda82b096608ae381076e12c79bc4c1bf
ALBATOR <SST.plomberie@laposte.net>
2021-12-04 21:12:37 +0100

    js les datas

---
+++ index.js
@@ -0,0 +1,6 @@
+const array = ["Paris","Lyon","Montreal","Bordeau"]
+const array1 = ["Paris", 33, ["Montreal","Bordeau"],true]
+
+for (i = 0; i < array1.length; i++){
+    console.log(array1[i]);
+}
